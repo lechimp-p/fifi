ed082134a0876d7280d832d57d4266cab5a2318e
Richard Klees <richard.klees@rwth-aachen.de>
2017-02-02 21:22:50 +0100

    init

---
+++ lib/fifi.ex
@@ -0,0 +1,18 @@
+defmodule Fifi do
+  @moduledoc """
+  Documentation for Fifi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fifi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fifi_test.exs
@@ -0,0 +1,8 @@
+defmodule FifiTest do
+  use ExUnit.Case
+  doctest Fifi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
