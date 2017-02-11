75d15062ca116202952017975d8cf518b2606eb9
Richard Klees <richard.klees@rwth-aachen.de>
2017-02-11 23:02:17 +0100

    made null-source a little more flexible for testing

--- lib/source/null.ex
+++ lib/source/null.ex
@@ -1,8 +1,17 @@
 defmodule Fifi.Source.Null do
-  defstruct id: ()
+  defstruct id: nil, description: nil, start_link: nil
 end
 
 defimpl Fifi.Source.Source, for: Fifi.Source.Null do
-  def description(_source), do: "A null source does nothing."
-  def start_link(_source, _frequency, _on_update), do: {:error, "Can't start null source."}
+  @default_description ("A null source does nothing")
+
+  def description(nil), do: @default_description
+  def description(source), do: source.description
+
+  def start_link(nil, _frequency, _on_update) do
+    {:error, "Can't start null source."}
+  end
+  def start_link(source, frequency, on_update) do
+    source.start_link.(frequency, on_update)
+  end
 end
