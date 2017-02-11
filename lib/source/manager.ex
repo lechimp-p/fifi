07af0371d6c62f4ecf67bb1e2de56346ed3c9032
Richard Klees <richard.klees@rwth-aachen.de>
2017-02-11 23:45:12 +0100

    start sources when listener is added to it.

--- lib/source/manager.ex
+++ lib/source/manager.ex
@@ -6,6 +6,10 @@ defmodule Fifi.Source.Manager do
   alias Fifi.Source.Registry, as: Registry 
   alias Fifi.Source.Source, as: Source 
 
+  @default_frequency 10_000
+
+  @type listener :: (any -> any)
+
   @doc """
   List all sources manager by the manager.
   """
@@ -17,17 +21,25 @@ defmodule Fifi.Source.Manager do
   @doc """
   Add a source to the manager.
   """
-  @spec add(PID, String.t, Source) :: :ok|:error
-  def add(manager, name, source) do
-    GenServer.call(manager, {:add, name, source})
+  @spec add_source(PID, String.t, Source) :: :ok|:error
+  def add_source(manager, name, source) do
+    GenServer.call(manager, {:add_source, name, source})
   end
 
   @doc """
   Remove a source from the manager.
   """
-  @spec remove(PID, String.t) :: :ok|:error
-  def remove(manager, name) do
-    GenServer.call(manager, {:remove, name})
+  @spec remove_source(PID, String.t) :: :ok|:error
+  def remove_source(manager, name) do
+    GenServer.call(manager, {:remove_source, name})
+  end
+
+  @doc """
+  Add a listener for a source.
+  """
+  @spec add_listener(PID, String.t, listener) :: {:ok, reference}|:error
+  def add_listener(manager, name, listener) do
+    GenServer.call(manager, {:add_listener, name, listener})
   end
 
   @doc """
@@ -49,12 +61,22 @@ defmodule Fifi.Source.Manager do
     {:reply, Registry.list(state.registry), state}
   end
 
-  def handle_call({:add, name, source}, _from, state) do
+  def handle_call({:add_source, name, source}, _from, state) do
     {:reply, Registry.add(state.registry, name, source), state}
   end
 
-  def handle_call({:remove, name}, _from, state) do
+  def handle_call({:remove_source, name}, _from, state) do
     {:reply, Registry.remove(state.registry, name), state}
+  end
+
+  def handle_call({:add_listener, name, _listener}, _from, state) do
+    case Registry.get(state.registry, name) do
+      :error -> {:reply, :error, state}
+      {:ok, source} -> case Source.start_link(source, @default_frequency, fn x -> IO.puts x end) do 
+        {:error, _reason} -> {:reply, :error, state}
+        {:ok, _pid} -> {:reply, {:ok, make_ref()}, state}
+      end
+    end
   end
 
   def handle_cast(_msg, state) do
--- test/source/manager_test.exs
+++ test/source/manager_test.exs
@@ -13,17 +13,41 @@ defmodule Fifi.Source.ManagerTest do
   test "list sources", %{manager: manager} do
     one = %Null{id: "one"}
     two = %Null{id: "two"}
-    Manager.add(manager,  "one", one)
-    Manager.add(manager, "two", two)
+    Manager.add_source(manager,  "one", one)
+    Manager.add_source(manager, "two", two)
     assert Manager.list(manager) == ["one", "two"]
   end
 
   test "remove source", %{manager: manager} do
     one = %Null{id: "one"}
     two = %Null{id: "two"}
-    Manager.add(manager,  "one", one)
-    Manager.add(manager, "two", two)
-    Manager.remove(manager, "two")
+    Manager.add_source(manager,  "one", one)
+    Manager.add_source(manager, "two", two)
+    Manager.remove_source(manager, "two")
     assert Manager.list(manager) == ["one"]
+  end
+
+  test "starts sources when listener is added", %{manager: manager} do
+    {:ok, called} = Agent.start(fn -> false end)
+    start_link = fn _f, _ou ->
+      Agent.update(called, fn _ -> true end)
+      {:ok, called}
+    end
+    one = %Null{start_link: start_link}
+    Manager.add_source(manager, "one", one)
+    assert not Agent.get(called, &(&1))
+    Manager.add_listener(manager, "one", fn _ -> :ok end)
+    assert Agent.get(called, &(&1))
+  end
+
+  test "can't add listener for non-existing source", %{manager: manager} do
+    assert Manager.add_listener(manager, "one", fn _ -> :ok end) == :error
+  end
+
+  test "get reference when adding listener", %{manager: manager} do
+    one = %Null{start_link: fn _f, _ou -> {:ok, self()} end}
+    Manager.add_source(manager, "one", one)
+    {:ok, ref} = Manager.add_listener(manager, "one", fn _ -> :ok end)
+    assert is_reference(ref)
   end
 end
